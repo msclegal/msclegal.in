c4d3a7d04739f71e3505a67dacd94e48d67becd5
msclegal <60327966+msclegal@users.noreply.github.com>
2020-02-01 20:25:57 +0530

    Update message.js

--- rw_common/assets/message.js
+++ rw_common/assets/message.js
@@ -1,5 +1,5 @@
 
-var rapidweaver_should_remember_cookie_setting=true;var rapidweaver_privacy_message_dismiss_button=document.getElementById('rapidweaver_privacy_message_dismiss_button');var rapidweaver_privacy_message_storage_name="has_shown_rapidweaver_privacy_message";rapidweaver_privacy_message_dismiss_button.onclick=function(){rapidweaver_hideMessage();rapidweaver_setCookieSetting(rapidweaver_privacy_message_storage_name,"true");};function rapidweaver_hideMessage(){var message=document.getElementById('rapidweaver_privacy_message');message.parentNode.removeChild(message);}
+var rapidweaver_should_remember_cookie_setting=true;var rapidweaver_privacy_message_dismiss_button=document.getElementById('rapidweaver_privacy_message_dismiss_button');var rapidweaver_privacy_message_storage_name="has_shown_rapidweaver_privacy_message";rapidweaver_privacy_message_dismiss_button.onclick=function(){rapidweaver_hideMessage();rapidweaver_setCookieSetting(rapidweaver_privacy_message_storage_name,"false");};function rapidweaver_hideMessage(){var message=document.getElementById('rapidweaver_privacy_message');message.parentNode.removeChild(message);}
 if(rapidweaver_getCookieSetting(rapidweaver_privacy_message_storage_name)=="true"){rapidweaver_hideMessage();}
 function rapidweaver_getCookieSetting(name){var storage=currentStorage();if(storage){return storage.getItem(name);}
 return null;}
